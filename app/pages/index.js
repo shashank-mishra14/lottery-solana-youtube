c0771f38098054ab29fe0e12e037ad156045b4d9
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-09-23 14:42:49 +0530

    setting up connection provider and wallet provider

--- app/pages/index.js
+++ app/pages/index.js
@@ -4,21 +4,28 @@ import Table from "../components/Table";
 import style from "../styles/Home.module.css";
 
 import { useMemo } from "react";
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function Home() {
-  const endpoint = "https://empty-icy-uranium.solana-devnet.discover.quiknode.pro/265fd54810637435a12cfb7108857693a7e8bb1d/";
+  const endpoint =
+    "https://empty-icy-uranium.solana-devnet.discover.quiknode.pro/265fd54810637435a12cfb7108857693a7e8bb1d/";
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
   return (
     <div className={style.wrapper}>
-      <Header />
-      <PotCard />
-      <Table />
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider>
+          <Header />
+          <PotCard />
+          <Table />
+        </WalletProvider>
+      </ConnectionProvider>
     </div>
   );
 }
-
