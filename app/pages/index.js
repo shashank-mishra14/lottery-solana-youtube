e44b2b0dafd2656cd591418206f7d0f8f4afee15
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-09-23 14:17:39 +0530

    setting up rpc provider

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,7 +3,15 @@ import PotCard from "../components/PotCard";
 import Table from "../components/Table";
 import style from "../styles/Home.module.css";
 
+import { useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
 export default function Home() {
+  const endpoint = useMemo(() => "https://empty-icy-uranium.solana-devnet.discover.quiknode.pro/265fd54810637435a12cfb7108857693a7e8bb1d/", []);
   return (
     <div className={style.wrapper}>
       <Header />
